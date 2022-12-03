dc8ce40482a034f5a33dc86da49e755db0f28d58
Nessa <la-nessa@mail.ru>
2022-12-03 19:56:52 +0300

    Ex6

---
+++ Ex6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+double number = Convert.ToInt32(Console.ReadLine());
+
+double x = number % 2;
+
+if (x == 0)
+{
+    Console.WriteLine("Четное число");
+}
+else
+{
+    Console.WriteLine("Нечетное число");
+}
