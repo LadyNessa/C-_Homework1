037b7b53567c2e30e72ee2c5ab7b9f69fd5136a6
Nessa <la-nessa@mail.ru>
2022-12-03 20:43:22 +0300

    Ex8

---
+++ Ex8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+double N =  Convert.ToInt32(Console.ReadLine());
+double x = 1;
+
+while (x <= N)
+{ 
+    if ((x % 2) == 0)
+    {
+    Console.WriteLine(x);
+    }
+    x++;
+}
+
