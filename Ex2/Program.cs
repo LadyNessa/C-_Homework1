dfb9bcc6de0bd5bbfac9b0b2d5ec947392326574
Nessa <la-nessa@mail.ru>
2022-12-03 18:22:45 +0300

    Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число: ");
+int number1 =  Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.Write("Max = ");
+    Console.WriteLine(number1);
+    Console.Write("Min = ");
+    Console.WriteLine(number2);
+}
+else
+{
+    Console.Write("Max = ");
+    Console.WriteLine(number2);
+    Console.Write("Min = ");
+    Console.WriteLine(number1);
+}
